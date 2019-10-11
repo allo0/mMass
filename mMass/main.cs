640b5d8248d03c4b19a8579dc3b9ad520ab7ad43
allo0 <ias.topalidis@gmail.com>
2019-10-11 09:36:03 +0300

    v0.01

---
+++ mMass/main.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+namespace mMass
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+            int y = -3;
+            int x = Math.Abs(y);
+
+            modBasics newMod = new modBasics();
+
+            newMod.move();
+            Console.WriteLine("Hellao World! {0}",x);
+        }
+    }
+}
---
+++ mMass/modBasics.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions; // re
+using System;// math(implemented in System)
+
+//stoper
+//obj_compound
+
+
+namespace mMass
+{
+    class modBasics
+    {
+        const double ELECTRON_MASS = 0.00054857990924;
+        const string FORMULA_PATTERN =@
+        public void move()
+        {
+            Console.WriteLine("HIIIII");
+        }
+    }
+}
