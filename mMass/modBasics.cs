eb35f864acd1a0572380c4c40d833068eca027b2
allo0 <ias.topalidis@gmail.com>
2019-10-21 11:10:02 +0300

    v0.02

--- mMass/modBasics.cs
+++ mMass/modBasics.cs
@@ -14,7 +14,7 @@ namespace mMass
 {
     class modBasics:obj_compound
     {
-        const float ELECTRON_MASS = 0.00054857990924f;
+        const double ELECTRON_MASS = 0.00054857990924d;
         const string FORMULA_PATTERN = @"^(
                                         ([\(])* # start parenthesis
                                         (
@@ -28,30 +28,32 @@ namespace mMass
                                           (?:\{([\d]+)\})? # isotope
                                          ([\-]?[\d]*) # atom count";
 
-      
+
 
         public void move()
         {
 
-           
+           double[] agentMass = new double[2];
+           string agentFormula = "H";
+           int agentCharge = 1;
 
-           
-            /* var base1 = new obj_compound();
-             var agentFormulas = base1.GetType();
-             Console.WriteLine("Is agetnFormulas an instance of BaseClass? {0}",
-                             agentFormulas.IsInstanceOfType(base1));
-             if ( !(agentFormulae=="e") && agentFormulas.IsInstanceOfType(base1))
+            obj_compound searcher = new obj_compound(); //for the isinstace
+            agentMass[0] = searcher.mass().Item1;
+            agentMass[1] = searcher.mass().Item2;
+            /* 
+             if (agentMass[1] == 0)
+                 agentMass[1] = ELECTRON_MASS;
+             if (agentMass[0] == 0)
+                 agentMass[0] = ELECTRON_MASS;
+             Console.WriteLine("...{0}...", agentMass[0]);
+             Console.WriteLine("...{0}...", agentMass[1]);
+             for (int i = 0; i < 2; i++)
              {
-                 Console.WriteLine("This is a detest" );
+                     agentMass[i] = agentMass[i] - agentCharge * ELECTRON_MASS;
+                   //  Console.WriteLine("...{0}...", agentMass[i]);
              }*/
-
-             obj_compound searcher = new obj_compound();
-             string agentFormulae = "H";
-            if (!(agentFormulae == "e") && !agentFormulae.Equals(searcher.hi()))
-                Console.WriteLine("holla {0}",
-                        searcher.hi().GetType() );
-
            
+
         }
 
       
@@ -79,8 +81,8 @@ namespace mMass
 
         }
     
-
-        public float mz(float mass, int charge, int currentCharge, string agentFormula, int agentCharge, bool massType) {
+        
+        public float mz(/*double mass,*/int charge, int currentCharge, string agentFormula, int agentCharge, int massType) {
             /*
                  """Calculate m/z value for given mass and charge.
                     mass (tuple of (Mo, Av) or float) - current mass
@@ -91,40 +93,68 @@ namespace mMass
                     massType (0 or 1)(false or true) - used mass type if mass value is float, 0(false) = monoisotopic, 1(true) = average
                 """
              */
-            
+           
+            double[] agentMass = new double[2];
+            double[] mass = new double[2]; 
             currentCharge = 0;
             agentFormula = "H";
             agentCharge = 1;
-            massType = false;
+            massType = 0;
             obj_compound searcher = new obj_compound(); //for the isinstace
 
-
-            if( !(agentFormula == "e") && !agentFormula.Equals(searcher.hi())   ) //isinstance comp
+            //check agent formula
+            if ( !(agentFormula == "e") && !agentFormula.Equals(searcher.func_meto_compound())   )   //isinstance comp
             {
-                //
+                agentFormula = searcher.func_meto_compound();   //
             }
+
+            // get agent mass
             if (agentFormula == "e")
             {
-                float[] agentMass = new float[1];
                 agentMass[0] = ELECTRON_MASS;
                 agentMass[1] = ELECTRON_MASS;
             }
             else
             {
+              
+               agentMass[0] = searcher.mass().Item1;
+               agentMass[1]= searcher.mass().Item2;
+                if (agentMass[1] == 0)
+                    agentMass[1] = ELECTRON_MASS;
+                if (agentMass[0] == 0)
+                    agentMass[0] = ELECTRON_MASS;
+
+                for (int i = 0; i < 2; i++)
+                {
+                    agentMass[i] = agentMass[i] - agentCharge * ELECTRON_MASS;
+                    //  Console.WriteLine("...{0}...", agentMass[i]);
+                }
 
             }
 
             int agentCount = currentCharge / agentCharge;
             if (currentCharge != 0)
             {
+                if () {
+                } // na to κανω αφου κανω τους Constructors στην obj_compound
+                else
+                    mass[0] = searcher.mass().Item1;
+                mass[1] = searcher.mass().Item2;
+                for(int i=0;i<2;i++)
+                 mass[i] = mass[i] * Math.Abs(currentCharge) - agentMass[massType] * agentCount;
+                 
+                //OR
+               // else
+               // mass = searcher.mass().Item1 + searcher.mass().Item2;
 
             }
-            
+            if(charge==0)
+                return
 
 
             return 0;
         }
-
+    
 
     }
 }
--- mMass/obj_compound.cs
+++ mMass/obj_compound.cs
@@ -6,12 +6,47 @@ namespace mMass
 {
     class obj_compound
     {
-        public string hi()
+        public obj_compound(string expression)
         {
-            string agentFormulae="H";
+            this.
+        }
+        public string func_meto_compound()
+        {
+            string agentFormulae = "Hollla";
 
-           return   agentFormulae;
-            
+            return agentFormulae;
+
+        }
+
+        public double he()
+        {
+            double[] agentMass = new double[2];
+            agentMass[0] = 1.123145;
+            agentMass[1] = 2.231416;
+            return agentMass[1];
+        }
+        public Tuple<double, double> mass()
+        {
+            int massType = 0;
+            double massMO = 1;
+            double massAV = 2;
+            var mass = Tuple.Create<double, double>(massMO,massAV);
+
+            if (massType == 0)
+            {
+                massAV = 0;
+                mass = Tuple.Create<double, double>(massMO, massAV);
+                return mass;
+            }
+            if (massType == 1)
+            {
+                massMO = 0;
+                mass = Tuple.Create<double, double>(massMO, massAV);
+                return mass;
+            }
+            else
+                return mass;
+
         }
     }
 }
